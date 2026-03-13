8bc77d0bc24681721a497ce6274a3c27390936ed
PasyaMaulana <pasyamaulana17@gmail.com>
2026-03-13 14:58:17 +0700

    CRUD (KTP)

---
+++ src/main/java/com/deploy/praktikum2crudktp/Praktikum2crudktpApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.praktikum2crudktp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum2crudktpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum2crudktpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/praktikum2crudktp/Praktikum2crudktpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.praktikum2crudktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum2crudktpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
